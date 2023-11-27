6a2dd95fe46d7a2bd6ec6cc2a75eee66aee65141
FindMalek <malekgarahellalbus@gmail.com>
2023-11-27 22:16:28 +0100

    :recycle: Refactored Navigation Links.

--- data/navigation.ts
+++ data/navigation.ts
@@ -1,6 +1,6 @@
 export const headerNavLinks = [
   ["À Propos", "/about"],
-  ["Fonctionnement", "#how-it-works"],
-  ["Avis", "#testimonials"],
-  ["FAQs", "#faqs"],
+  ["Fonctionnement", "/#how-it-works"],
+  ["Avis", "/#testimonials"],
+  ["FAQs", "/#faqs"],
 ];
