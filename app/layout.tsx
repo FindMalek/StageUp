dbc0e2ce19220d7c391b030285f2a40f7be3d09d
FindMalek <malekgarahellalbus@gmail.com>
2023-11-27 22:31:38 +0100

    :construction_worker:Added Link Parsing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,21 @@ export const metadata: Metadata = {
   title: "StageUp - Your Professional Community",
   description:
     "StageUp is a professional community for students and recent graduates to connect with each other and with companies.",
+  openGraph: {
+    title: "StageUp - Your Professional Community",
+    description:
+      "StageUp is a professional community for students and recent graduates to connect with each other and with companies.",
+    url: "https://stageup.vercel.app/",
+    siteName: "StageUp",
+    type: "website",
+    images: "https://stageup.vercel.app/og.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "StageUp - Your Professional Community",
+    description:
+      "StageUp is a professional community for students and recent graduates to connect with each other and with companies.",
+  },
 };
 
 export default function RootLayout({
