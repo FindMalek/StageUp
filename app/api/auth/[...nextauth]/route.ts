623df75b18e9b30f4d869796b853f10f33be603e
FindMalek <malekgarahellalbus@gmail.com>
2023-11-28 12:10:56 +0100

    :sparkles:Implementing NextAuth.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+});
