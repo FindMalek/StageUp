ffa0b54838695fbad3317a79dcadddd880519f44
FindMalek <malekgarahellalbus@gmail.com>
2023-12-08 15:19:23 +0100

    :building_construction:Complete changes in the App Router | Pages Structure.

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,50 @@
+import "@styles/globals.css";
+
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+
+import { Toaster } from "@/components/ui/Toaster";
+import ProvidersLayout from "@/components/sections/authentification/ProviderLayout";
+
+import Header from "@/components/sections/navigation/Header";
+import Footer from "@/components/sections/navigation/Footer";
+
+export const metadata: Metadata = {
+  title: "StageUp - Your Professional Community",
+  description:
+    "StageUp is a professional community for students and recent graduates to connect with each other and with companies.",
+  openGraph: {
+    title: "StageUp - Your Professional Community",
+    description:
+      "StageUp is a professional community for students and recent graduates to connect with each other and with companies.",
+    url: "https://stageup.vercel.app/",
+    siteName: "StageUp",
+    type: "website",
+    images: "https://stageup.vercel.app/og.jpeg",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "StageUp - Your Professional Community",
+    description:
+      "StageUp is a professional community for students and recent graduates to connect with each other and with companies.",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={GeistSans.className}>
+        <ProvidersLayout>
+          <Header />
+          {children}
+          <Footer />
+          <Toaster />
+        </ProvidersLayout>
+      </body>
+    </html>
+  );
+}
