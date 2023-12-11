2b2babdf8b67f00d624ee3ada9f160d920f809eb
FindMalek <malekgarahellalbus@gmail.com>
2023-12-12 00:02:43 +0100

    :camera_flash:Update Herosection image.

--- components/sections/display/Hero.tsx
+++ components/sections/display/Hero.tsx
@@ -70,7 +70,7 @@ export default function Herosection() {
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <img
-                src="/image/sections/herosection.webp"
+                src="/image/sections/hero.png"
                 alt="A temporary screen shot from LinkedIn."
                 width={2432}
                 height={1442}
